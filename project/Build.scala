dd5022ad5b6e9674f1c0048c42260a79cd5ba185
Carlos Ferreyra <cferreyra@despegar.com>
2014-06-18 19:50:27 -0300

    Added scalatra-swagger dependency

--- project/Build.scala
+++ project/Build.scala
@@ -21,14 +21,26 @@ object Build extends Build {
       resolvers ++= Seq(
         "Sonatype OSS Releases"  at "http://oss.sonatype.org/content/repositories/releases/"
       )
-    )
+    ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings
 
-  lazy val main = Project("main", file("."))
+  lazy val main = Project("tests", file("."))
     .dependsOn(macrosSub)
     .settings(commonSettings: _*)
     .settings(
       libraryDependencies ++= Seq(
-        "com.wordnik"       %   "swagger-core_2.10"     % "1.3.6",
+        "com.wordnik"       %   "swagger-core_2.10"     % "1.3.+"   % "test"
+                exclude("com.fasterxml.jackson.core",     "jackson-annotations")
+                exclude("com.fasterxml.jackson.jaxrs",    "jackson-jaxrs-json-provider")
+                exclude("com.fasterxml.jackson.module",   "jackson-module-jsonSchema")
+                exclude("com.fasterxml.jackson.module",   "jackson-module-scala_2.10")
+                exclude("com.wordnik",                    "swagger-annotations")
+                exclude("commons-lang",                   "commons-lang")
+                exclude("org.json4s",                     "json4s-ext_2.10")
+                exclude("org.json4s",                     "json4s-jackson_2.10")
+                exclude("org.json4s",                     "json4s-native_2.10"),
+        "org.scalatra"      %   "scalatra-swagger_2.10" % "2.3.+"   % "provided"
+                exclude("com.typesafe.akka", "akka-actor")
+                exclude("com.wordnik",                    "swagger-core_2.9.1"),
         "org.scalatest"     %%  "scalatest"             % "2.1.7"   % "test"
       )
     )
@@ -38,7 +50,19 @@ object Build extends Build {
     .settings(
       libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
       libraryDependencies ++= Seq(
-        "com.wordnik"       %   "swagger-core_2.10"     % "1.3.6"
+        "com.wordnik"       %   "swagger-core_2.10"     % "1.3.+"   % "provided"
+                exclude("com.fasterxml.jackson.core",     "jackson-annotations")
+                exclude("com.fasterxml.jackson.jaxrs",    "jackson-jaxrs-json-provider")
+                exclude("com.fasterxml.jackson.module",   "jackson-module-jsonSchema")
+                exclude("com.fasterxml.jackson.module",   "jackson-module-scala_2.10")
+                exclude("com.wordnik",                    "swagger-annotations")
+                exclude("commons-lang",                   "commons-lang")
+                exclude("org.json4s",                     "json4s-ext_2.10")
+                exclude("org.json4s",                     "json4s-jackson_2.10")
+                exclude("org.json4s",                     "json4s-native_2.10"),
+        "org.scalatra"      %   "scalatra-swagger_2.10" % "2.3.+"   % "provided"
+                exclude("com.typesafe.akka", "akka-actor")
+                exclude("com.wordnik",                    "swagger-core_2.9.1")
       ),
       unmanagedSourceDirectories in Compile <+= (scalaVersion, sourceDirectory in Compile) {
         case (v, dir) if v startsWith "2.10" => dir / "scala_2.10"
